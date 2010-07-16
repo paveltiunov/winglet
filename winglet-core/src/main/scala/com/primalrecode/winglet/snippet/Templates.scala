80408b5789ffb8d3514bbf459f18f72cae9cbfb6
Pavel Tiunov <pavel.tiunov@gmail.com>
2010-07-16 19:23:26 +0400

    Templates editing works through ajax

--- winglet-core/src/main/scala/com/primalrecode/winglet/snippet/Templates.scala
+++ winglet-core/src/main/scala/com/primalrecode/winglet/snippet/Templates.scala
@@ -1,11 +1,11 @@
 package com.primalrecode.winglet.snippet
 
 import com.primalrecode.winglet.{ModelAccess}
-import xml.NodeSeq
 import net.liftweb.http.{SHtml, StatefulSnippet}
 import com.primalrecode.winglet.model.Template
 import net.liftweb.util.BindHelpers
 import net.liftweb.http.js.{JE, JsCmds}
+import xml.{Elem, Text, NodeSeq}
 
 class Templates extends StatefulSnippet with ModelAccess with BindHelpers {
   private var uri:String = ""
@@ -15,30 +15,36 @@ class Templates extends StatefulSnippet with ModelAccess with BindHelpers {
   private var templateListId = "templateList"
 
   def dispatch = {
-    case "createForm" => createForm _
     case "templates" => templates _
   }
 
-  def createForm(in:NodeSeq) = bind(
+  def createForm(templateListTemplate:NodeSeq)(in:NodeSeq) = SHtml.ajaxForm(bind(
     "f", in,
     "uri" -%> SHtml.text(uri, uri = _, "id" -> uriFieldId),
     "content" -%> SHtml.textarea(content, content = _, "id" -> contentFieldId),
-    "submit" -%> SHtml.submit("Save", createTemplate)
-  )
+    "submit" -%> SHtml.ajaxSubmit("Save", () => {
+      createTemplate
+      templateListRefreshCmd(templateListTemplate)
+    })
+  ))
 
-  def templates(in:NodeSeq):NodeSeq = <div id={templateListId}>{templatesContent(in)}</div>
+  def templates(in:NodeSeq) = bind("t", in, "templateList" -%> templatesList _, "editForm" -%> createForm(chooseTemplate("t","templateList",in)) _)
+
+  def templatesList(in:NodeSeq):Elem = <div id={templateListId}>{templatesContent(in)}</div>
 
   def templatesContent(in:NodeSeq):NodeSeq = Template.allTemplates.flatMap(t => bind(
     "t", in,
-    "edit" -> SHtml.a(<div>{t.uri}</div>) {
+    "edit" -> SHtml.a(Text(t.uri)) {
       val template = Template.reload(t)
       JsCmds.SetValById(uriFieldId, JE.strToS(template.uri)) & JsCmds.SetValById(contentFieldId, JE.strToS(template.content))
     },
-    "remove" -> SHtml.a(<div>Remove</div>) {
+    "remove" -%> {linkText => SHtml.a(linkText) {
       Template.remove(t)
-      JsCmds.SetHtml(templateListId, templatesContent(in))
-    }
+      templateListRefreshCmd(in)
+    }}
   ))
+
+  def templateListRefreshCmd(templateListTemplate:NodeSeq) = JsCmds.SetHtml(templateListId, templatesContent(templateListTemplate))
 
   def createTemplate() = Template.create(uri, content)
 }
