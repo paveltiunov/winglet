26f62b0d776cf654b117c69562fe0b72699d4009
Pavel Tiunov <pavel.tiunov@gmail.com>
2010-07-16 20:26:36 +0400

    Sitemap redesign

--- winglet-core/src/main/scala/com/primalrecode/winglet/config/SiteMapGen.scala
+++ winglet-core/src/main/scala/com/primalrecode/winglet/config/SiteMapGen.scala
@@ -5,13 +5,16 @@ import com.google.inject.Inject
 import com.primalrecode.winglet.Model
 import com.primalrecode.winglet.model.Page
 import com.primalrecode.winglet.view.UriHelpers
+import net.liftweb.sitemap.Loc._
 
 class SiteMapGen @Inject()(model: Model) extends UriHelpers {
   def generate(): SiteMap = {
     var entries = Menu(Loc("Home", List("index"), "Home")) ::
-            Menu(Loc("Add page", List("admin", "addpage"), "Add page")) ::
-            Menu(Loc("Add template", List("admin", "addtemplate"), "Add template")) ::
-            Menu(Loc("Upload resource", List("admin", "uploadresource"), "Upload resource")) ::
+            Menu(Loc("Admin", List("admin", "index"), "Administration"),
+              Menu(Loc("Pages", List("admin", "pages"), "Pages", LocGroup("admin"), Hidden)),
+              Menu(Loc("Templates", List("admin", "templates"), "Templates", LocGroup("admin"), Hidden)),
+              Menu(Loc("Resources", List("admin", "resources"), "Resources", LocGroup("admin"), Hidden))
+            ) ::
             Nil
 //    val pages = model.findAll[Page]("allPages")
 //    entries ++= pages.filter(p => p.name != null && splitUri(p.url).length > 0).map(p => Menu(Loc(p.name, splitUri(p.url), p.name)))
